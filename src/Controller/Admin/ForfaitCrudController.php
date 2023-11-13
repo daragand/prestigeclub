f1e1f03be5b27e82d0eb7814385c58cc6ea0f382
Demba Thiam <daragan.dooms@gmail.com>
2023-11-13 09:50:01 +0100

    Ajout du Crud admin pour le forfait

---
+++ src/Controller/Admin/ForfaitCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Forfait;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ForfaitCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Forfait::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
