af166f1b56412f4d59e88410c849f2286e8fcec7
Demba Thiam <daragan.dooms@gmail.com>
2023-11-13 12:42:09 +0100

    personnalisation des titres de pages du Crud Adresse

--- src/Controller/Admin/AddressCrudController.php
+++ src/Controller/Admin/AddressCrudController.php
@@ -3,16 +3,29 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Address;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class AddressCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Address::class;
+    }
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPageTitle('index', 'Adresses')
+            ->setPageTitle('new', 'Ajouter une adresse')
+            ->setPageTitle('edit', 'Modifier une adresse')
+            ->setPageTitle('detail', 'Détail de l\'adresse')
+            ->setSearchFields(['city', 'adress', 'zip'])
+            ->setDefaultSort(['id' => 'DESC'])
+            ->setEntityLabelInSingular('Adresse')
+            ->setEntityLabelInPlural('Adresses');
     }
 
     /*
