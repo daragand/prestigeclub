e0ddc7062ca7c7d074430a4933c80d6b774d55b4
Demba Thiam <daragan.dooms@gmail.com>
2023-11-15 12:19:53 +0100

    test sur controller Photo

--- src/Controller/PhotoController.php
+++ src/Controller/PhotoController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Form\PhotoType;
 use App\Entity\Licencie;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,11 +20,23 @@ class PhotoController extends AbstractController
         ]);
     }
     #[Route('/ajout-photo', name: 'app_photo_ajout')]
-    public function addPhoto(): Response
+    public function addPhoto(
+        Request $request, EntityManagerInterface $entityManager
+    ): Response
     {
         $licencie = new Licencie();
 
         $formAddPhoto = $this->createForm(PhotoType::class, $licencie);
+        //si le formulaire est valide
+        $formAddPhoto->handleRequest($request);
+
+        if ($formAddPhoto->isSubmitted() && $formAddPhoto->isValid()) {
+
+
+        //ajout du licencié et de ses photos dans la base de données
+        $entityManager->persist($licencie);
+        $entityManager->flush();
+        }
 
         return $this->render('photo/ajoutphoto.html.twig', [
             'controller_name' => 'PhotoController',
