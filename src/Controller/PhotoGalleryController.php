e1015eaab5eb9a33d1aabb4f5018c1cd43aa4d6f
Demba Thiam <daragan.dooms@gmail.com>
2023-11-21 11:22:48 +0100

    💄 ajout des assets css et JS 🚧 Début de la mise en place du controller presentation

--- src/Controller/PhotoGalleryController.php
+++ src/Controller/PhotoGalleryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PhotoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class PhotoGalleryController extends AbstractController
 {
     #[Route('/photo/gallery', name: 'app_photo_gallery')]
-    public function index(): Response
+    public function presentation(PhotoRepository $photoRepository): Response
     {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $roles = $this->getUser()->getRoles();
+
+        if (in_array('ROLE_USER', $roles)) {
+            
+
+            $user = $this->getUser();
+        $licencies = $user->getLicencies();
+
+        $photos = $photoRepository->findBy();
+        }
+        
+        
+
         return $this->render('photo_gallery/gallery.html.twig', [
             'controller_name' => 'PhotoGalleryController',
         ]);
