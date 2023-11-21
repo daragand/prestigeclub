dbae94df8bff8fb4b6a293755d8cd18f33fdfb54
Demba Thiam <daragan.dooms@gmail.com>
2023-11-21 10:26:42 +0100

    ✨ création d'un controller phtoGallery pour permettre aux parents d'accéder aux photos

---
+++ src/Controller/PhotoGalleryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PhotoGalleryController extends AbstractController
+{
+    #[Route('/photo/gallery', name: 'app_photo_gallery')]
+    public function index(): Response
+    {
+        return $this->render('photo_gallery/index.html.twig', [
+            'controller_name' => 'PhotoGalleryController',
+        ]);
+    }
+}
