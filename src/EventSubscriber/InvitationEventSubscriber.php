5ed519feb162fca11d5ff40729484436fb9f961f
Demba Thiam <daragan.dooms@gmail.com>
2023-11-22 09:55:51 +0100

    Ajout du paramètre de l'email d'envoi fonctionnel. Ajout d'un fichier pour le template email en cours

--- src/EventSubscriber/InvitationEventSubscriber.php
+++ src/EventSubscriber/InvitationEventSubscriber.php
@@ -4,16 +4,28 @@ namespace App\EventSubscriber;
 
 use App\Entity\Licencie;
 use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityPersistedEvent;
 
+
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
 class InvitationEventSubscriber implements EventSubscriberInterface
 {
-    public function __construct(string $emailSender)
+    private $mailer;
+    private $container;
+    private $parameterBag;
+
+    public function __construct(MailerInterface $mailer,  ParameterBagInterface $parameterBag)
     {
-        $this->emailSender = $senderEmail;
+        $this->mailer = $mailer;
+       
+        $this->parameterBag = $parameterBag;
     }
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -21,7 +33,7 @@ class InvitationEventSubscriber implements EventSubscriberInterface
         ];
     }
 
-    public function sendInvitationEmail(AfterEntityPersistedEvent $event, MailerInterface $mailer)
+    public function sendInvitationEmail(AfterEntityPersistedEvent $event)
     {
         $entity = $event->getEntityInstance();
 
@@ -30,13 +42,24 @@ class InvitationEventSubscriber implements EventSubscriberInterface
         }
 
         $emailLicencie = $entity->getEmail();
-        $mailSender=$this->getParameters('email_sender');
+        //récupération de l'adresse mail de l'expéditeur dans le fichier .yaml (récupéré lui-même dans le fichier .env)
+        $mailSender = $this->parameterBag->get('email_address');
+        
+        dd($entity);
+        
 
-        $email = (new Email())
-            ->from($container->getParameter('email_sender'));
+        $email = (new TemplatedEmail())
+            ->from($mailSender)
+            ->to($entity->getEmail())
+            ->subject('Confirmation de votre inscription')
+            ->htmlTemplate('mail/licence/confirmation.html.twig')
+            ->context([
+                'licence' => $entity,
+            ]);
 
+            $mailer->send($email);
 
     }
 }
 
-
+        
