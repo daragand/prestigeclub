f42e8ded4fa9d50a8561c709c9897a05e89d7e46
Demba Thiam <daragan.dooms@gmail.com>
2023-11-22 18:57:15 +0100

    modification texte pour email dans le pied de page Modification de la couleur arrière plan dans les coordonnées

--- src/EventSubscriber/InvitationEventSubscriber.php
+++ src/EventSubscriber/InvitationEventSubscriber.php
@@ -69,6 +69,7 @@ $domain = $this->parameterBag->get('domain');
             ->htmlTemplate('mail/invitation.html.twig')
             ->context([
                 'uuid' => $uuid,
+                'emailcontact' => $mailSender,
                 'emailLicencie' => $emailLicencie,
                 'domain' => $domain,
                 'firstName' => $firstName,
