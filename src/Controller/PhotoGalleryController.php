738bd1a420922e72a218b2018678608a600cb8d9
Demba Thiam <daragan.dooms@gmail.com>
2023-11-29 16:23:24 +0100

    problème dans la récupération du Licencie à cause de la relation en ManyToMany

--- src/Controller/PhotoGalleryController.php
+++ src/Controller/PhotoGalleryController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Licencie;
+use App\Entity\User;
+use App\Repository\LicencieRepository;
+use App\Repository\UserRepository;
 use App\Repository\PhotoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,30 +14,38 @@ use Symfony\Component\Routing\Annotation\Route;
 class PhotoGalleryController extends AbstractController
 {
     #[Route('/photo/gallery', name: 'app_photo_gallery')]
-    public function presentation(PhotoRepository $photoRepository): Response
+    public function presentation(
+        PhotoRepository $photoRepository,
+        UserRepository $userRepository,
+        LicencieRepository $licencieRepository
+        ): Response
     {
-        // if (!$this->getUser()) {
-        //     return $this->redirectToRoute('app_login');
-        // }
-
-        // $roles = $this->getUser()->getRoles();
-
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+        $user = $this->getUser();
+        $roles = $user->getRoles();
+        $licencie = $licencieRepository->findBy(['users' => $this->getUser()]);
+    //    $licencies = $licencieRepository->findBy(['users' => $this->getUser()]);
+            dd($licencies);
         /**
-         * Si l'utilisateur est un usager, on affiche les photos liées à son compte
+         * Si l'utilisateur est un usager, on affiche les photos liées à ses licenciés
          */
-        // if (in_array('ROLE_USER', $roles)) {
+        if ($roles[0] === 'ROLE_USER') {
+            // $licencies = $user->getLicencies();
             
-
-        //     $user = $this->getUser();
-        //     $photos = $photoRepository->findBy(['user' => $user]);
-
-        
-        // }
+          
+            $photos = $photoRepository->findBy(['users' => $this->getUser()]);
+        } else {
+            return $this->redirectToRoute('admin');
+        }
+        dd($photos);
         
         
 
         return $this->render('photo_gallery/gallery.html.twig', [
             'controller_name' => 'PhotoGalleryController',
+            'photos' => $photos
         ]);
     }
 
