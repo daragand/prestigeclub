eb0629aa46308ba287c3648e972eeaf3538441a9
Demba Thiam <daragan.dooms@gmail.com>
2023-11-30 12:04:34 +0100

    essai de liipImagine - watermark non fonctionnel ajout d'une route pour l'accueil des usagers qui ont plusieurs enfants sportifs

--- src/Controller/PhotoGalleryController.php
+++ src/Controller/PhotoGalleryController.php
@@ -13,6 +13,62 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PhotoGalleryController extends AbstractController
 {
+    #[Route('/photos', name: 'app_photos_index')]
+    public function index(LicencieRepository $licencieRepository, UserRepository $userRepository, PhotoRepository $photoRepository): Response
+    {
+
+        /**
+         * Si l'usager n'est pas pas connecté, je le redirige vers la page de connexion par sécurité.
+         */
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        /**
+         * Je récupère l'adresse email de l'utilisateur connecté, pour récupérer son profil Utilisateur
+         */
+        $userConnected = $this->getUser();
+        $email = $userConnected->getUserIdentifier();
+
+        /**
+         * Je récupère le profil Utilisateur de l'utilisateur connecté
+         */
+        $user = $userRepository->findOneBy(['email' => $email]);
+
+        
+        $userLicencies = $user->getLicencies();
+        
+        
+        foreach ($userLicencies as $userLicencie) {
+            
+            $photos[$userLicencie->getId()] = $photoRepository->findBy(['licencie' => $userLicencie]);
+        }
+           
+        /**
+         * Si l'utilisateur est un usager, on affiche les photos liées à ses licenciés
+         */
+        // if ($roles[0] === 'ROLE_USER') {
+        //     // $licencies = $user->getLicencies();
+            
+          
+        //     $photos = $photoRepository->findBy(['users' => $this->getUser()]);
+        // } else {
+        //     return $this->redirectToRoute('admin');
+        // }
+        // dd($photos);
+        
+        $photos = array_merge(...$photos);
+
+        return $this->render('photo_gallery/index.html.twig', [
+            'controller_name' => 'PhotoGalleryController',
+            'photos' => $photos,
+            'licencie' => $userLicencie,
+        ]);
+    }
+    
+    
+    
+    
     #[Route('/photo/gallery', name: 'app_photo_gallery')]
     public function presentation(
         PhotoRepository $photoRepository,
@@ -32,7 +88,7 @@ class PhotoGalleryController extends AbstractController
         $user = $userRepository->findOneBy(['email' => $email]);
         $userLicencies = $user->getLicencies();
         
-        
+        //ajouter une fonction IN ARRAY
         foreach ($userLicencies as $userLicencie) {
             
             $photos[] = $photoRepository->findBy(['licencie' => $userLicencie]);
@@ -55,7 +111,8 @@ class PhotoGalleryController extends AbstractController
 
         return $this->render('photo_gallery/gallery.html.twig', [
             'controller_name' => 'PhotoGalleryController',
-            'photos' => $photos
+            'photos' => $photos,
+            'licencie' => $userLicencie,
         ]);
     }
 
