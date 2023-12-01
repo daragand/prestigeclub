456ee62760a928466591b744fc56c99981a33c38
Demba Thiam <daragan.dooms@gmail.com>
2023-12-01 12:43:42 +0100

    Ajout des options dans le Dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -9,6 +9,7 @@ use App\Entity\Livret;
 use App\Entity\Address;
 use App\Entity\Forfait;
 use App\Entity\Licencie;
+use App\Entity\Options;
 use App\Entity\PhotoGroup;
 use App\Repository\LivretRepository;
 use App\Repository\OrderRepository;
@@ -111,6 +112,7 @@ public function configureCrud(): Crud
         yield MenuItem::section('Gestion')->setCssClass('border-bottom border-2');
         yield MenuItem::linkToCrud('Adresses', 'fas fa-map-marker-alt', Address::class);
         yield MenuItem::linkToCrud('Forfaits', 'fas fa-money-bill-wave', Forfait::class);
+        yield MenuItem::linkToCrud('Options', 'fas fa-cog', Options::class);
         yield MenuItem::linkToCrud('Clubs','fa-solid fa-landmark', Club::class);
         yield MenuItem::linkToCrud('Licenciés', 'fas fa-users', Licencie::class)
         ;
