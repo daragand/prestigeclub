acb08665e0ca8d4a33bc26b75d94df48abca0588
Demba Thiam <daragan.dooms@gmail.com>
2023-12-06 17:59:49 +0100

    Add request handling in PanierController and update form action in gallery.html.twig

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -3,14 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PanierController extends AbstractController
 {
     #[Route('/panier', name: 'app_panier')]
-    public function index(): Response
+    public function index(
+        Request $request,
+    ): Response
     {
+        $keys=$request->request->all();
+        //récupération des clés du tableau
+        $keys=array_keys($keys);
+        
+        dd($keys);
+
+       
+
         return $this->render('panier/index.html.twig', [
             'controller_name' => 'PanierController',
         ]);
