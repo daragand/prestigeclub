a3a51ed65d75bbaaeab4c9d3a30db7770d0487bf
Demba Thiam <daragan.dooms@gmail.com>
2023-12-19 21:25:38 +0100

    Add photo group to zip file creation

--- src/Service/zipdownload.php
+++ src/Service/zipdownload.php
@@ -1,37 +1,50 @@
 <?php
 
 namespace App\services;
-
+use App\Entity\PhotoGroup;
+use App\Entity\Photo;
 use App\Entity\Order;
+use Vich\UploaderBundle\Storage\FileSystemStorage;
+use Vich\UploaderBundle\Storage\StorageInterface;
 
 
 class ZipDownload
 {
-    public function zipCreate(Order $order)
+    public static function zipCreate(Order $order)
     {
         $zip = new \ZipArchive();
-        $zipName = 'zip/' . $order->getId().'_' .$order->getUsers()->getLastname(). '_'. '.zip';
+        $zipName = 'zip_' . $order->getId().'_' .$order->getUsers()->getLastname(). '_'. '.zip';
 
         //récupération des photos en fonction du forfait
         if($order->getForfait()){
             switch($order->getForfait()->getName()){
                 case 'Gratuite':
-                    $photoGroupe = $order->getPhotos()->slice(0, 10);
+                    $photoGroupe = $order->getLicencie()->getGroupes()->getPhotoGroup();
+                    $photos = [];
                     break;
                 case 'Champion':
-                    $photos = $order->getPhotos()->slice(0, 1);
+                    $photos = $order->getLicencie()->getPhotos()->slice(0, 1);
+                    $photoGroupe = $order->getLicencie()->getGroupes()->getPhotoGroup();
                     break;
                 case 'Prestige':
-                    $photos = $order->getPhotos()->slice(0, 30);
+                    $photos = $order->getLicencie()->getPhotos()->slice(0, 3);
+                    $photoGroupe = $order->getLicencie()->getGroupes()->getPhotoGroup();
                     break;
                 default:
-                    $photos = $order->getPhotos()->slice(0, 10);
+                    $photos = [];
                     break;
                 
                 
 
         }
     }
+//ajout de photo de groupe si existant
+    if($photoGroupe){
+        $zip->open($zipName, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
+        $zip->addFile('uploads/photos/' . $photoGroupe->getPhotoGroupFile(), $photoGroupe->getPhoto());
+        $zip->close();
+    }
+
 
     //si les photos sont existantes, on les ajoute au zip
     if (count($photos) > 0) {
@@ -40,6 +53,7 @@ class ZipDownload
         foreach ($photos as $photo) {
             $zip->addFile('uploads/photos/' . $photo->getPhoto(), $photo->getPhoto());
         }
+
         $zip->close();
     }
         return $zipName;
