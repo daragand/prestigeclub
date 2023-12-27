f105e9e674625171522b8d3430a8dc141a1ae1c2
Demba Thiam <daragan.dooms@gmail.com>
2023-12-27 13:17:30 +0100

    Add user profile edit functionality

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,6 +23,29 @@ class ProfilController extends AbstractController
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
             'user' => $user,
+        ]);
+    }
+    #[Route('/profil/edit', name: 'app_profil_edit')]
+    public function edit(Request $request,EntityManagerInterface $entityManager): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+        $user = $this->getUser();
+        $formUser = $this->createForm(UserType::class, $user);
+        $formUser->handleRequest($request);
+
+        if ($formUser->isSubmitted() && $formUser->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_profil');
+
+        }
+        
+        return $this->render('profil/edit.html.twig', [
+            'controller_name' => 'ProfilController',
+            'user' => $user,
+            'formUser' => $formUser->createView(),
         ]);
     }
 }
