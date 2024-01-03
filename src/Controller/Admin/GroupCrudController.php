b8917e1b0e4c91dcc2e631aa3516ab63c038b6e5
Demba Thiam <daragan.dooms@gmail.com>
2024-01-03 16:31:20 +0100

    Add permissions to hide edit, new, and delete buttons in Group detail page

--- src/Controller/Admin/GroupCrudController.php
+++ src/Controller/Admin/GroupCrudController.php
@@ -4,17 +4,29 @@ namespace App\Controller\Admin;
 
 use App\Entity\Group;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class GroupCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Group::class;
+    }
+    public function configureActions(Actions $actions): Actions
+    {
+
+    return $actions
+        ->add(Crud::PAGE_INDEX, Action::DETAIL)
+       //permet de masquer le bouton edit, new et delete dans la page detail pour le Club
+        ->setPermissions([Action::NEW => 'ROLE_ADMIN', 
+        Action::DELETE => 'ROLE_ADMIN',
+         Action::EDIT => 'ROLE_ADMIN']);
     }
 public function configureCrud(Crud $crud): Crud{
     return $crud
