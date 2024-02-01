5427386ea3af39f020a83be4e8754fb79b0b3e99
Demba Thiam <daragan.dooms@gmail.com>
2024-02-01 21:54:22 +0100

    Le paramètre de la route app_order_confirmation modifié pour uuidOrder

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -42,19 +42,19 @@ class PaymentController extends AbstractController
         }
 
         //pour contourner le Lazy Loading(objet reconnu que par l'id), on récupère le forfait en tant que référence
-        $forfaitName= $cart->getForfait()->getName();
+        $forfaitName = $cart->getForfait()->getName();
 
-        
-       /**
-        * Lors de la récupération du panier, il semble que le forfait ne veut pas apparaitre. Utilisation donc du get_Reference.
-        * A terme, il faudra trouver une solution pour que le forfait apparaisse lors de la récupération du panier sans alourdir le code de la sorte.
-        */
+
+        /**
+         * Lors de la récupération du panier, il semble que le forfait ne veut pas apparaitre. Utilisation donc du get_Reference.
+         * A terme, il faudra trouver une solution pour que le forfait apparaisse lors de la récupération du panier sans alourdir le code de la sorte.
+         */
 
         $forfait = $this->entityManager->getReference(Forfait::class, $cart->getForfait()->getId());
-        
-        
 
-       
+
+
+
 
         $productStripe = [];
 
@@ -72,33 +72,33 @@ class PaymentController extends AbstractController
                 ];
             }
         }
-        
-        if($cart->getForfait()){
-           
+
+        if ($cart->getForfait()) {
+
             $productStripe[] = [
                 'price_data' => [
                     'currency' => 'eur',
                     'unit_amount' => $forfait->getPrice() * 100,
                     'product_data' => [
-                        'name' => 'forfait '.$forfaitName,
+                        'name' => 'forfait ' . $forfaitName,
                     ],
                 ],
                 'quantity' => 1,
             ];
         }
 
-        if(count($productStripe) === 0){
+        if (count($productStripe) === 0) {
             $this->addFlash('error', 'Votre panier est vide et ne peut donc pas faire l\objet d\'un paiement.');
             return $this->redirectToRoute('app_panier_visualiser');
         }
 
-        
+
         $stripeSecretKey = $this->parameterBag->get('secret_key_stripe');
 
         Stripe::setApiKey($stripeSecretKey);
         header('Content-Type: application/json');
 
-       
+
 
 
 
@@ -119,13 +119,12 @@ class PaymentController extends AbstractController
     #[Route('/order/success/{id}', name: 'app_payment_success')]
     public function success(
         Cart $cart,
-         UserRepository $userRepository,
-         ZipDownload $zip,
-            MailingService $mailingService
-         ): RedirectResponse
-    {
+        UserRepository $userRepository,
+        ZipDownload $zip,
+        MailingService $mailingService
+    ): RedirectResponse {
 
-        
+
         //création d'une commande depuis le panier
         $order = new Order();
         $order->setAmount($cart->getAmount())
@@ -134,70 +133,74 @@ class PaymentController extends AbstractController
             ->setLicencie($cart->getLicencie())
             ->setUuidOrder(uniqid());
 
-            //modification de la date du licencié concerné par la commande
-         $order->getLicencie()->setUpdatedAt(new \DateTime());
+        //modification de la date du licencié concerné par la commande
+        $order->getLicencie()->setUpdatedAt(new \DateTime());
 
-        if($cart->getOptionLists()) {
+        if ($cart->getOptionLists()) {
             foreach ($cart->getOptionLists() as $optionList) {
                 $order->addOptionList($optionList);
             }
         }
         //ajout du forfait dans la commande depuis le panier
-        if($cart->getForfait()) {
-            
+        if ($cart->getForfait()) {
+
             $order->setForfait($cart->getForfait());
         }
         /**
          * Pour la gestion du statut de la commande. S'il s'agit d'une commande de forfait Prestige, le statut est automatiquement mis à "en attente de validation"
          */
-         //si c'est un forfait prestige ou qu'une option a été ajoutée, le statut est "en cours de traitement" pour gérer le livret
-         if($cart->getForfait()->getName() == 'Prestige'||$cart->getOptionLists()){
+        //si c'est un forfait prestige ou qu'une option a été ajoutée, le statut est "en cours de traitement" pour gérer le livret
+        if ($cart->getForfait()->getName() == 'Prestige' || $cart->getOptionLists()) {
             $orderStatus = $this->entityManager->getRepository(OrderStatus::class)->findOneBy(['name' => 'En cours de traitement']);
             $order->setOrderStatus($orderStatus);
             // Pour le forfait prestige, j'ajoute les photos du panier, dans la commande
-                if($cart->getForfait()->getName() == 'Prestige'){
-                    foreach ($cart->getPhotos() as $photo) {
-                        $order->addPhoto($photo);
-                    }
+            if ($cart->getForfait()->getName() == 'Prestige') {
+                foreach ($cart->getPhotos() as $photo) {
+                    $order->addPhoto($photo);
                 }
-            }elseif($cart->getForfait()->getName() == 'Champion'){
-                $orderStatus = $this->entityManager->getRepository(OrderStatus::class)->findOneBy(['name' => 'Payée']);
-                $order->setOrderStatus($orderStatus);
             }
+        } elseif ($cart->getForfait()->getName() == 'Champion') {
+            $orderStatus = $this->entityManager->getRepository(OrderStatus::class)->findOneBy(['name' => 'Payée']);
+            $order->setOrderStatus($orderStatus);
 
- 
-            $zipFile = $zip->zipCreate($order);
+            //ajout des photos sélectionnées par l'usager signalée dans le panier dans la commande
+            foreach ($cart->getPhotos() as $photo) {
+                $order->addPhoto($photo);
+            }
+            dd($order->getPhotos());
+        }
+
+
+        $zipFile = $zip->zipCreate($order);
         $order->setZipFile($zipFile);
-        
-       
+
+
         //suppression du panier (placement à null les relations) et enregistrement de la commande
         $this->entityManager->persist($order);
-        
-        if($cart->getOptionLists()){
+
+        if ($cart->getOptionLists()) {
             foreach ($cart->getOptionLists() as $optionList) {
                 $optionList->setCart(null);
-                
             }
-        }elseif($cart->getForfait()){
+        } elseif ($cart->getForfait()) {
             $cart->setForfait(null);
-        }elseif($cart->getPhotos()){
+        } elseif ($cart->getPhotos()) {
             foreach ($cart->getPhotos() as $photo) {
                 $photo->removeCart($cart);
             }
-        }elseif($cart->getUsers()){
+        } elseif ($cart->getUsers()) {
             $cart->setUsers(null);
         }
         $this->entityManager->remove($cart);
-        
-        
-        
+
+
+
         $this->entityManager->flush();
 
         //envoi du mail de téléchargement des photos
-        $mailingService->downloadPhoto($order,$zipFile);
-        
-       return $this->redirectToRoute('app_order_confirmation', ['id' => $order->getId()]);
-     
+        $mailingService->downloadPhoto($order, $zipFile);
+
+        return $this->redirectToRoute('app_order_confirmation', ['uuidOrder' => $order->getUuidOrder()]);
     }
     #[Route('/order/error/{id}', name: 'app_payment_error')]
     public function error(): RedirectResponse
@@ -206,6 +209,4 @@ class PaymentController extends AbstractController
         $this->addFlash('error', 'Le règlement de votre commande a échoué. N\'hésitez pas à réessayer.');
         return $this->redirectToRoute('app_panier_visualiser');
     }
-
-    
 }
